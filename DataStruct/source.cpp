f262b9c6be302f234bc81b611c081d2fd447be89
bitss <donrang@naver.com>
2024-10-15 23:01:07 +0900

    신동민 : myvector의 오류 테스트

--- DataStruct/source.cpp
+++ DataStruct/source.cpp
@@ -1,12 +1,96 @@
 #include <vector>
-#include "vector.h"
+// #include "vector.h"
 #include <crtdbg.h>
+#include "myvec.h"
+#include <iostream>
+
+class myclass
+{
+public:
+	myclass()
+		: ptr(nullptr)
+	{
+		std::cout << "생성자 호출 시작" << std::endl;
+
+		ptr = new int;
+
+		++callCountConstructor;
+
+		std::cout << "생성자 호출 종료, count : " << callCountConstructor << std::endl;
+	}
+	~myclass()
+	{
+		std::cout << "소멸자 호출 시작" << std::endl;
+
+		if (ptr != nullptr)
+		{
+			delete ptr;
+			ptr = nullptr;
+		}
+
+		++callCountDestructor;
+
+		std::cout << "소멸자 호출 종료, count : " << callCountDestructor << std::endl;
+	}
+
+	myclass(const myclass& other)
+	{
+		std::cout << "복사 생성자 호출" << std::endl;
+
+		ptr = new int;
+
+		++callCountCopyConstructor;
+
+		std::cout << "복사 생성자 호출 종료, count : " << callCountCopyConstructor << std::endl;
+	}
+
+private:
+	int* ptr;
+
+	volatile static size_t callCountConstructor;
+	volatile static size_t callCountCopyConstructor;
+	volatile static size_t callCountDestructor;
+};
+
+volatile size_t myclass::callCountCopyConstructor = 0;
+volatile size_t myclass::callCountConstructor = 0;
+volatile size_t myclass::callCountDestructor = 0;
 
 int main()
 {
+	//_CrtSetDbgFlag(_CRTDBG_ALLOC_MEM_DF | _CRTDBG_LEAK_CHECK_DF);
+
+	////func();
+
+	//UVector<myclass> vec = {};
+
+	//vec.reserve(3);
+
+	//for (size_t i = 0; i < 100; ++i)
+	//{
+	//	vec.push_back(myclass());
+	//	std::cout << "i : " << i << std::endl;
+	//}
+
+	//return 0;
+
+
+
 	_CrtSetDbgFlag(_CRTDBG_ALLOC_MEM_DF | _CRTDBG_LEAK_CHECK_DF);
 
-	func();
+	//func();
 
+	UVector<myclass> vec = {};
+
+	// vec.reserve(1);
+
+	for (size_t i = 0; i < 10; ++i)
+	{
+		std::cout << "i : " << i << " (for문 실행 시작)" << std::endl;
+		vec.push_back(myclass());
+		printf_s("i : %d, vec.size : %d, vec.capacity : %d \n", i, vec.size(), vec.capacity());
+	}
+
+	std::cout << "main 함수 종료" << std::endl;
 	return 0;
 }
--- DataStruct/vector.h
+++ DataStruct/vector.h
@@ -2,41 +2,44 @@
 #include <assert.h>
 #include <vector>
 
-// template <typename T>
+//typedef float T;
+template <typename T>
 class MyVector
 {
 public:
 	MyVector()
 		: data(nullptr)
 		, count(0)
-		, max_count(1)
+		, max_count(0)
 	{
-		data = new float[max_count];
 	}
 	~MyVector()
 	{
-		Release();
+		if (data != nullptr)
+		{
+			delete[] data;
+			data = nullptr;
+		}
 	}
 
 private:
-	float* data;
+	T* data;
 	size_t count;
 	size_t max_count;
 
 public:
-	float& operator[] (size_t _index) const { return data[_index]; }
-	float& at(size_t _index) const
+	T& at(size_t _index) const
 	{
 		if (_index >= count)
 			assert(nullptr);
 
 		return data[_index];
 	}
-	size_t capacity()
+	size_t capacity() const
 	{
 		return max_count;
 	}
-	size_t size()
+	size_t size() const
 	{
 		return count;
 	}
@@ -45,38 +48,68 @@ public:
 		count = 0;
 	}
 
-	void push_back(const float& _data)
+	void push_back(const T& _data)
 	{
 		if (count >= max_count)
-			resize(max_count * 2);
+		{
+			size_t value = max_count * 2 <= 0 ? 1 : max_count * 2;
+			reserve(value);
+		}
 
 		data[count] = _data;
 		++count;
 	}
 
-	void resize(size_t _capacity)
+	void reserve(size_t _capacity)
 	{
-		float* tempvector = new float[_capacity];
-		
-		for (size_t i = 0; i < count; ++i)
-			tempvector[i] = data[i];
+		if (_capacity <= max_count)
+			return;
 
-		delete data;
-		data = tempvector;
+		T* tempvector = data;
+		data = new T[_capacity];
+
+		for (size_t i = 0; i < count; ++i)
+			data[i] = tempvector[i];
+
+		if (tempvector != nullptr)
+		{
+			delete[] tempvector;
+			tempvector = nullptr;
+		}
 		max_count = _capacity;
+
+#ifdef _DEBUG
+		printf_s("데이터 재 할당\n");
+#endif // DEBUG
+	}
+
+	T& operator[] (size_t _index) const
+	{
+		return data[_index];
+	}
+
+};
+
+class myclass
+{
+public:
+	myclass()
+		: ptr(nullptr)
+	{
+		ptr = new int;
+	}
+	~myclass()
+	{
+		if (ptr != nullptr)
+		{
+			delete ptr;
+			ptr = nullptr;
+		}
 	}
 
 private:
-	void Release()
-	{
-		if (data != nullptr)
-		{
-			delete data;
-			data = nullptr;
-		}
-	}
+	int* ptr;
 };
-
 void func()
 {
 	std::vector<float> vec = {};
@@ -85,13 +118,17 @@ void func()
 	vec.reserve(1);
 	vec.resize(1);
 
-	MyVector myvec = {};
-	
+	MyVector<myclass> myvec = {};
+
+	printf_s("value = %s, size = %d, capacity = %d\n", "not found data", myvec.size(), myvec.capacity());
+
+	myvec.reserve(3);
+
 	for (size_t i = 0; i < 100; i++)
-		myvec.push_back(static_cast<float>(i));
-	
-	for (size_t i = 0; i < 100; i++)
-		printf_s("%f\n", myvec[i]);
+	{
+		myvec.push_back(myclass());
+		printf_s("value = %f, size = %d, capacity = %d\n", (myvec[i]), myvec.size(), myvec.capacity());
+	}
 	
 	return;
 }
